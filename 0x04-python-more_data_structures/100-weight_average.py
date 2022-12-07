2d985b98ff4e0528bc845e2c7d92298fa7dc8040
karrey <109669003+karreycodes@users.noreply.github.com>
2022-12-07 18:23:41 +0300

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list) == 0:
-        return 0
-
-    return sum([mul(x[0], x[1]) for x in my_list]) / sum(x[1] for x in my_list)
-
-def mul(x, y):
-    return x * y
+    if my_list and len(my_list):
+        num = 0
+        denom = 0
+        for tup in my_list:
+            num += (tup[0] * tup[1])
+            denom += (tup[1])
+        return (num/denom)
+    return 0
