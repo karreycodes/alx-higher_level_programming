462aed77a1be0880c1e4f502dd768adfdadf98c6
karrey <109669003+karreycodes@users.noreply.github.com>
2023-01-10 15:50:59 +0300

    a script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-"""This is the documentation for this module. On this file we have to
-write a JSON rep of an object to a file"""
+"""Module 7-save_to_json_file.
+Writes an Object to a text file,
+using a JSON representation.
+"""
+
+
+import json
 
 
 def save_to_json_file(my_obj, filename):
-    with open(filename, 'w') as archivito:
-        import json
-        atta = json.dumps(my_obj)
-        archivito.write(atta)
+    """Writes the representation of my_obj
+    to filename.
+    Args:
+        - my_obj: object to write
+        - filename: file to write into
+    """
+
+    with open(filename, 'w+') as f:
+        json.dump(my_obj, f)
