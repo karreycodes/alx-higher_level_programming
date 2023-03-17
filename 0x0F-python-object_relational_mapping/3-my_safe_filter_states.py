1dca77bf44f485b64f80c79ac567a6f373f402af
karrey <109669003+karreycodes@users.noreply.github.com>
2023-03-17 16:11:12 +0300

    safe from MySQL injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """
-This script lists all cities from
-the database `hbtn_0e_4_usa`.
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+
+This time the script is safe from
+MySQL injections!
 """
 
 import MySQLdb
@@ -9,7 +14,7 @@ from sys import argv
 
 if __name__ == '__main__':
     """
-    Access to the database and get the cities
+    Access to the database and get the states
     from the database.
     """
 
@@ -19,16 +24,16 @@ if __name__ == '__main__':
     with db.cursor() as cur:
         cur.execute("""
             SELECT
-                cities.id, cities.name, states.name
+                *
             FROM
-                cities
-            JOIN
                 states
-            ON
-                cities.state_id = states.id
+            WHERE
+                name LIKE BINARY %(name)s
             ORDER BY
-                cities.id ASC
-        """)
+                states.id ASC
+        """, {
+            'name': argv[4]
+        })
 
         rows = cur.fetchall()
 
