e37b19521f620ebe3d0827108e6db5a26259bf6b
karrey <109669003+karreycodes@users.noreply.github.com>
2023-04-26 08:57:15 +0300

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,36 @@
 #!/usr/bin/node
 
-const request = require('request');
+// Import the `fs` module. The `fs` module provides functions for working with files.
 const fs = require('fs');
+
+// Import the `request` module. The `request` module provides functions for making HTTP requests.
+const request = require('request');
+
+// Get the URL from the command line arguments.
 const url = process.argv[2];
+
+// Get the file path from the command line arguments.
 const filePath = process.argv[3];
 
-request(url, function (err, response, body) {
+// Make an HTTP GET request to the URL.
+request(url, (err, res, body) => {
+  // Check if there was an error.
   if (err) {
+    // If there was an error, log it to the console and exit.
     console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
+    return;
   }
+
+  // Write the body of the response to the file in the UTF-8 encoding.
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    // Check if there was an error.
+    if (err) {
+      // If there was an error, log it to the console and exit.
+      console.log(err);
+      return;
+    }
+
+    // The file has been written successfully.
+    console.log(`File ${filePath} has been written successfully.`);
+  });
 });
